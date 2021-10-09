923fe5a6143549f8c04b39e3421b5cd659bf685e
andycedillo <andycedillo@gmail.com>
2021-10-09 12:06:33 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,74 +1,87 @@
-// an array to add multiple particles
+
 let particles = [];
-let img;
-let kittens = [];
-let countZoom;
+let cells = [];
+let sf = 1; 
+let x = 0;
+let y = 0; 
+let mx, my; 
+let flagMove;
+let  randomImg;
 
 
 function preload() {
+    for (let i = 1; i < 11; i++){
+        cells[i - 1] = loadImage('assets/cell-(' + i + ').jpg');
+    }
 
-
-    for (let i = 1; i < 80; i++)
-        kittens[i] = loadImage('assets/cell-(' + i + ').jpg');
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-
+    flagMove = 0;
     for (let i = 0; i < width / 4; i++) {
         particles.push(new Particle());
     }
 }
 
 function draw() {
+    mx = mouseX;
+    my = mouseY;
     background(0, 0, 100);
-    push();
-    //translate(width / 2, height / 2);
-    //scale(4);
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].createParticle();
-        //particles[i].moveParticle();
-        particles[i].joinParticles(particles.slice(i));
-    }
-    pop();
-}
+    translate(mx, my);
+    scale(sf);
+    translate(-mx, -my);
 
-window.onresize = function() {
-    // assigns new values for width and height variables
-    w = window.innerWidth;
-    h = window.innerHeight;
-    canvas.size(w, h);
-}
 
-function mouseWheel(event) {
-    print(event.delta);
-    //move the square according to the vertical scroll amount
-    countZoom += map(event.delta, 0, 1, 0, height);
-    //uncomment to block page scrolling
-    //return false;
+    if (mouseIsPressed) {
+        x -= pmouseX - mouseX;
+        y -= pmouseY - mouseY;
+      }
+
+ 
+        for (let i = 0; i < particles.length; i++) {
+            particles[i].createParticle();
+            //particles[i].moveParticle();
+            particles[i].joinParticles(particles.slice(i));
+        }
+
 }
 
 
-// this class describes the properties of a single particle.
+
+window.addEventListener("wheel", function(e) {
+    if (e.deltaY > 0)
+      sf *= 1.05;
+    else
+      sf *= 0.95;
+  });
+
+
+
 class Particle {
-    // setting the co-ordinates, radius and the
-    // speed of a particle in both the co-ordinates axes.
     constructor() {
         this.x = random(0, width);
         this.y = random(0, height);
-        this.r = 30
+        this.r = random(0,50);
         this.xSpeed = random(-2, 2);
         this.ySpeed = random(-1, 1.5);
-        this.kitten = random(kittens);
+        this.cell = random(cells);
 
     }
 
     // creation of a particle.
     createParticle() {
         noStroke();
-        fill('rgba(200,169,169,0.5)');
-        //circle(this.x, this.y, this.r);
-        image(this.kitten, this.x, this.y, this.r, this.r);
+        if(sf <= 1){
+            fill('rgba(200,169,169,0.5)');
+            circle(this.x, this.y, this.r);
+        }if(sf > 1){
+            image(this.cell, this.x, this.y, this.r, this.r);
+            console.log(this.cell);
+
+        }
+  
+   
     }
 
     // setting the particle in motion.
@@ -81,12 +94,11 @@ class Particle {
         this.y += this.ySpeed;
     }
 
-    // this function creates the connections(lines)
-    // between particles which are less than a certain distance apart
+
     joinParticles(particles) {
         particles.forEach(element => {
             let dis = dist(this.x, this.y, element.x, element.y);
-            if (dis < 100) {
+            if (dis < 60) {
                 stroke('rgba(255,255,255');
                 line(this.x, this.y, element.x, element.y);
             }
